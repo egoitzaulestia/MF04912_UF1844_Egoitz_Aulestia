dbb3f32e959a228d8eb8b8ce710efeb2df5cf769
egoitzaulestia <egoitzaulestia@gmail.com>
2024-06-10 09:47:02 +0200

    near to finish

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
-<div class="row">
-    <div class="col-12" id="footer">
+<div class="row footer-bg">
+    <div class="col-12 footer-text" id="footer">
         &copy; 2024 <?php echo $alumno ?>
     </div>
 </div>
